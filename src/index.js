38356fa64fabd46730e1358f6c67995eea20dd93
CircuitSyn <solderedcircuit@gmail.com>
2019-10-23 00:08:36 -0700

    commented additions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render } from "react-dom";
+// import { render } from "react-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import gallery components for main page
-import Gallery from "react-photo-gallery";
-import { photos } from "./photos";
+// import Gallery from "react-photo-gallery";
+// import { photos } from "./photos";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
