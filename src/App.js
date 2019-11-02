7a0ac143bb97ba561c547ba3945d5c7fc59418dd
CircuitSyn <circuitsyn@gmail.com>
2019-11-01 21:52:04 -0700

    added basic navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+// import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import NavBar from './components/NavBar/NavBar';
+// import gallery components for main page
 import Gallery from "react-photo-gallery";
 import { photos } from "./photos";
 // import { render } from "react-dom";
 import './App.css';
-// import gallery components for main page
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <Gallery photos={photos} />
       </div>
     );
