75cffec348660e2f687cfa63152859ef8bb0301e
CircuitSyn <circuitsyn@gmail.com>
2019-11-07 19:34:38 -0800

    adjusted margin for title of page and also fixed menu pop up

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,17 +9,17 @@ const NavBar = (props) => {
         <a href="/" className="navbar-brand titleFont"><span className="titleOr">Jarred Sutton</span> | Photography</a>
         
         
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navBarToggleTrig" aria-controls="navBarToggleTrig" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navBarToggleTrig" aria-controls="navBarToggleTrig" aria-expanded="false" aria-label="Toggle navigation" data-display="static">
           <img className="navIcon" src={navIcon} alt="camera" />
         </button>
       </nav>
 
         {/* Directory Components */}
         <div className="collapse text-right" id="navBarToggleTrig">
-          <div className="bg-light p-4">
+          <div className="bg-light pb-4 pr-4 pt-0">
             <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
               <li className="nav-item active">
-                <a className="nav-link" href="/Home">Home</a>
+                <a className="nav-link" href="/">Home</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="/Blog">Blog</a>
@@ -28,7 +28,7 @@ const NavBar = (props) => {
                 <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   Galleries
                 </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <div className="dropdown-menu dropdown-menu-right dropdown-menu-xl-left" aria-labelledby="navbarDropdown">
                   <a className="dropdown-item" href="/Birds">Birds</a>
                   <a className="dropdown-item" href="/Cetaceans">Cetaceans</a>
                   <a className="dropdown-item" href="/Insects">Insects</a>
