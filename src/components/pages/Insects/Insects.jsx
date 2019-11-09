770d7751c8c29f1ccb28e668e9b9136896c27adc
CircuitSyn <circuitsyn@gmail.com>
2019-11-08 20:01:38 -0800

    lightbox added to insects

--- src/components/pages/Insects/Insects.jsx
+++ src/components/pages/Insects/Insects.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState, useCallback } from "react";
 import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
 import { insectPhotos } from "./insectPhotos";
 
 const Insects = (props) => {
 
+  // functions to facilitate lightbox
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
   return (
     <div>
-      <Gallery photos={insectPhotos} />
+      <Gallery photos={insectPhotos} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={insectPhotos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
     </div>
   );
 }
