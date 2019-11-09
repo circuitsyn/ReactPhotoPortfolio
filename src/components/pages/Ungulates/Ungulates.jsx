56e412fe3b5fd9353f1034665f9462b8926bb8df
CircuitSyn <circuitsyn@gmail.com>
2019-11-08 22:13:11 -0800

    added lightbox to ungulaes and winter

--- src/components/pages/Ungulates/Ungulates.jsx
+++ src/components/pages/Ungulates/Ungulates.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState, useCallback } from "react";
 import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
 import { ungulatePhotos } from "./ungulatePhotos";
 
 const Ungulates = (props) => {
 
+  // functions to facilitate lightbox
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
   return (
     <div>
-      <Gallery photos={ungulatePhotos} />
+      <Gallery photos={ungulatePhotos} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={ungulatePhotos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
     </div>
   );
 }
--- src/components/pages/Winter/Winter.jsx
+++ src/components/pages/Winter/Winter.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState, useCallback } from "react";
 import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
 import { winterPhotos } from "./winterPhotos";
 
 const Winter = (props) => {
 
-  return (
-    <div>
-      <Gallery photos={winterPhotos} />
-    </div>
-  );
-}
+    // functions to facilitate lightbox
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+  
+    const openLightbox = useCallback((event, { photo, index }) => {
+      setCurrentImage(index);
+      setViewerIsOpen(true);
+    }, []);
+  
+    const closeLightbox = () => {
+      setCurrentImage(0);
+      setViewerIsOpen(false);
+    };
+
+    return (
+      <div>
+        <Gallery photos={winterPhotos} onClick={openLightbox} />
+        <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={winterPhotos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+      </div>
+    );
+  }
 
 export default Winter;
