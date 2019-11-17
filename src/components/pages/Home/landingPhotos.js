d7da3723127c92157e4b8be74520b521a35a6748
CircuitSyn <circuitsyn@gmail.com>
2019-11-17 02:54:10 -0800

    added updated pixels to images for native ratio and pulled in github security fixes

--- src/components/pages/Home/landingPhotos.js
+++ src/components/pages/Home/landingPhotos.js
@@ -1,64 +1,64 @@
 export const landingPhotos = [
     {
       src: '/imgs/landing/birds.jpg',
-      width: 4,
-      height: 3,
+      width: 500,
+      height: 333,
       alt: "Birds",
       overlay: './imgs/landing/birdsTitle.jpg'
     },
     {
       src: "/imgs/landing/cetaceans.jpg",
-      width: 4, 
-      height: 3,
+      width: 500, 
+      height: 333,
       alt: "Cetaceans",
       overlay:  './imgs/landing/cetaceansTitle.jpg'
     },
     {
       src: "/imgs/landing/insects.jpg",
-      width: 4,
-      height: 3,
+      width: 500,
+      height: 332,
       alt: "Insects",
       overlay: './imgs/landing/insectsTitle.jpg' 
     },
     {
       src: "/imgs/landing/landscapes.jpg",
-      width: 4,
-      height: 3,
+      width: 500,
+      height: 332,
       alt: "Landscapes",
       overlay: './imgs/landing/landscapesTitle.jpg' 
     },
     {
       src: "/imgs/landing/nightTime.jpg",
-      width: 3,
-      height: 5,
+      width: 333,
+      height: 500,
       alt: "Night-Sky",
       overlay: './imgs/landing/nightTimeTitle.jpg'
     },
     {
       src: "/imgs/landing/ungulates.jpg",
-      width: 4,
-      height: 3,
+      width: 500,
+      height: 375,
       alt: "Ungulates",
       overlay: './imgs/landing/ungulatesTitle.jpg' 
     },
     {
       src: "/imgs/landing/waterFeatures.jpg",
-      width: 4,
-      height: 5,
+      width: 333,
+      height: 500,
       alt: "Water-Features",
       overlay: './imgs/landing/waterFeaturesTitle.jpg'
     },
     {
       src: "/imgs/landing/winter.jpg",
-      width: 4,
-      height: 3,
+      width: 500,
+      height: 333,
       alt: "Winter",
       overlay: './imgs/landing/winterTitle.jpg' 
     },
     {
       src: "./imgs/landing/panoramas.jpg",
-      width: 4927,
-      height: 1000,
+      width: 2500,
+      height: 439,
       alt: "Panoramas",
       overlay: './imgs/landing/panoramasTitle.jpg' 
     },
@@ -71,22 +71,22 @@ export const landingPhotos = [
     },
     {
       src: "./imgs/landing/travel.jpg",
-      width: 4,
-      height: 5,
+      width: 750,
+      height: 1000,
       alt: "Travel",
       overlay: './imgs/landing/travelTitle.jpg' 
     },
     {
       src: "./imgs/landing/seaLife.jpg",
-      width: 4,
-      height: 3,
+      width: 1000,
+      height: 669,
       alt: "SeaLife",
       overlay: './imgs/landing/seaLifeTitle.jpg'
     }, 
     {
       src: "./imgs/landing/reptiles.jpg",
-      width: 4,
-      height: 3,
+      width: 1000,
+      height: 669,
       alt: "Reptiles",
       overlay: './imgs/landing/reptilesTitles.jpg'
     }
