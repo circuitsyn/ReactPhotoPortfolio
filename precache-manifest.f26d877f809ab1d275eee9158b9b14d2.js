8c50c0f5838c3a2042ea1a6f8c208e85fa9071cf
CircuitSyn <circuitsyn@gmail.com>
2019-12-10 09:52:12 -0800

    Updates

---
+++ precache-manifest.f26d877f809ab1d275eee9158b9b14d2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "07cbbf9a383b2853de2426beed27305b",
+    "url": "/static/media/jarred-sutton-travel.07cbbf9a.jpg"
+  },
+  {
+    "revision": "d87b8b4503f902dc7933",
+    "url": "/static/css/main.631e9eea.chunk.css"
+  },
+  {
+    "revision": "1068e4d0f48847ae02fb1ed9da793cdf",
+    "url": "/static/media/Feldn___.1068e4d0.woff"
+  },
+  {
+    "revision": "dcf9bb15d30898652acc",
+    "url": "/static/js/1.dcf9bb15.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4cf3be0cf8bf36ea18428d010e2a1b9a",
+    "url": "/static/media/jarredSutton.4cf3be0c.jpg"
+  },
+  {
+    "revision": "1d95a45b19dbeada5340c03a205ffe38",
+    "url": "/static/media/jarred-sutton-teaching.1d95a45b.jpg"
+  },
+  {
+    "revision": "d87b8b4503f902dc7933",
+    "url": "/static/js/main.d87b8b45.chunk.js"
+  },
+  {
+    "revision": "9fdd7ae8f4dfbf003b35f6d6c5a8278f",
+    "url": "/static/media/loading.9fdd7ae8.gif"
+  },
+  {
+    "revision": "1f9b1828f1a0ff53130710a02aa2ce24",
+    "url": "/static/media/panoramas.1f9b1828.jpg"
+  },
+  {
+    "revision": "6d076cd30358b41085e34ac4d49eb02a",
+    "url": "/static/media/copyrightImg.6d076cd3.png"
+  },
+  {
+    "revision": "f43211c9a87a1f642bbb4e3fcb893688",
+    "url": "/static/media/blogSunset.f43211c9.jpg"
+  },
+  {
+    "revision": "dcf9bb15d30898652acc",
+    "url": "/static/css/1.44f19768.chunk.css"
+  },
+  {
+    "revision": "17d9e6019812871d0ed091162a870add",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ReactPhotoPortfolio/precache-manifest.efe71ba85fc470c368bf364e1dc4f6c9.js"
+  "/precache-manifest.f26d877f809ab1d275eee9158b9b14d2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ReactPhotoPortfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
