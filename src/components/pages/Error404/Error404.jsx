a10396d17452dc3ed3a63210fd76b011e8232308
CircuitSyn <circuitsyn@gmail.com>
2019-12-10 22:39:48 -0800

    styled error page

--- src/components/pages/Error404/Error404.jsx
+++ src/components/pages/Error404/Error404.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './styleError.css';
 import Image404 from './404robot.png';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image'
+import Image from 'react-bootstrap/Image';
 
 const Error404 = (props) => {
 
@@ -15,12 +16,19 @@ const Error404 = (props) => {
         <Container>
             <Row>
                 <Col>
-                    <h1>Error 404: Whoops!</h1>
+                    <h1 id="errorTitle" className="m-1">Error 404: Whoops!</h1>
                 </Col>
             </Row>
             <Row>
                 <Col>
-                    <Image src={Image404} alt="404 robot" rounded />
+                    <Image id="img404" className="m-3" src={Image404} alt="404 robot" rounded />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <p>
+                        Looks like you reached a URL with no content. No worries! Click on my Jarred Sutton Photography Brand link in the top left to take you home! If this keeps happening please do drop me a line and <a className="errorLink" href="/contact">let me know</a>, thanks!
+                    </p>
                 </Col>
             </Row>
         </Container>
