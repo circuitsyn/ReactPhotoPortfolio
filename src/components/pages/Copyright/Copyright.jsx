4cbbd2d0d27f6f49e9d8d0d591752e6ab1de7966
CircuitSyn <circuitsyn@gmail.com>
2019-12-11 12:12:08 -0800

    copyright styling page edits

--- src/components/pages/Copyright/Copyright.jsx
+++ src/components/pages/Copyright/Copyright.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
             <Container className="mt-5 mb-5">
                 <Row>
                     <Col>
-                        <img className="float-left copyRImg mr-2 mb-2" src={CopyrightImg} alt="copyright" />
+                        <img className="float-left copyRImg mr-2 mb-1" src={CopyrightImg} alt="copyright" />
                         <h1 className="text-left titleFont">Copyright Info:</h1>
                         <p className="text-left">
                         In the litigious society that we live it is important to note how we view the art and creative work we create. With all of my photography and work I reserve all rights as the creator and photographer. I am more than happy to discuss usage, teaching/shooting rates, or sale of my work in various mediums. Please do feel free to reach out and drop me a message via my contact page or via social media. I will do my best to get back to you asap! Please do not download or use my images without my consent. As with all artists it takes a great amount of time, effort, and money to produce our work the way we do. Thank you so much for visiting and happy shooting!
@@ -29,27 +29,30 @@ const Contact = () => {
             <Container className="mt-5">
                 <Row>
                     <Col>
-                        <img className="float-left copyRImg mr-2 mb-2" src={CreditsImg} alt="credits" />
+                        <img className="float-left cRListImg mr-2 mb-1" src={CreditsImg} alt="credits" />
                         <h3 className="text-left titleFont mb-0">Credits:</h3>
-                        <p className="text-left small text-muted">
-                        Credits for items not created by myself are also important and i wanted to make sur eto give credit where credit is due. If anything is missing please reach out and let me know, thank you!
+                        <p className="text-left">
+                        Credits for items not created by myself are also important and I wanted to make sure to give credit where credit is due. If anything is missing please reach out and <a className="errorLink" href="/contact">let me know</a>, thank you!
                         </p>
                     </Col>
                 </Row>
-                <Row>
-                    <Col sm={12} md={12} lg={12} xl={12}>
-                        <ul className="d-flex align-items-center text-left flex-column">
+                <Row className="d-flex justify-content-center text-left">
+                    <Col sm={7} md={7} lg={7} xl={7}>
+                        <ul className="">
                             <li>
-                                <strong>Copyright logo</strong> - <div>Made by <a href="https://www.flaticon.com/authors/itim2101" title="itim2101">itim2101</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                                <strong>Copyright logo</strong> - Made by <a href="https://www.flaticon.com/authors/itim2101" title="itim2101">itim2101</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
                             </li>
                             <li>
-                                <strong>Navigation camera logo</strong> - <div>Made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                                <strong>Navigation camera logo</strong> - Made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
                             </li>
                             <li>
-                                <strong>Credits logo</strong> - <div>Made by <a href="https://www.flaticon.com/authors/becris" title="Becris">Becris</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                                <strong>Credits logo</strong> - Made by <a href="https://www.flaticon.com/authors/becris" title="Becris">Becris</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
                             </li>
                             <li>
-                                Font - 
+                                <strong>Error 404 robot logo</strong> - from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+                            </li>
+                            <li>
+                                <strong>Styled font</strong> - Feldicouth Norm
                             </li>
                         </ul>
                     </Col>
