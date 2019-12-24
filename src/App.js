bd381a037095308ec461f00c7daeed41b7d9af68
CircuitSyn <circuitsyn@gmail.com>
2019-12-23 23:17:48 -0500

    removed browser router import for clean up from app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, withRouter } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer'
