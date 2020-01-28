dbaf82cc7961038e42d0a4d66780ec082904f5f1
CircuitSyn <circuitsyn@gmail.com>
2020-01-28 15:51:04 -0800

    added test function to initial ste

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -2,6 +2,9 @@ export default {
     blog: {
       loading: false,
       posts: [],
-      articleNum: 0
+      articleNum: 0,
+      alterArticleState: () => {
+        console.log('Altered the article state!');
+      } 
     }
   }
