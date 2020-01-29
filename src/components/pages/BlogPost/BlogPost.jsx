68941ec6ecdca7bfb4b51a137ec647a743bed280
CircuitSyn <circuitsyn@gmail.com>
2020-01-28 21:04:47 -0800

    added loading to blogPost to protect against failure to load and also added banner jumbo tron with date

--- src/components/pages/BlogPost/BlogPost.jsx
+++ src/components/pages/BlogPost/BlogPost.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { connect } from 'react-redux';
+import moment from 'moment';
+import * as Markdown from 'react-markdown';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import Loading from '../../Loader/Loader';
 
 class BlogPost extends React.Component {
   constructor(props) {
@@ -17,16 +20,42 @@ class BlogPost extends React.Component {
     this.props.blog.alterArticleState();
   }
 
+  // loading function to alternate between loading state and data present state if needed
+  loading = () => {
+    
+    // conditional to test and see if data is available to fill posts
+    if (this.props.blog.posts.length === 0) {
 
-  render() {
+      return(<Loading />)
+    }
+    else {
 
-    return (
-      <div>
-        <Jumbotron id="blogJumbotron" style={{backgroundImage: `url(${this.props.blog.posts[this.props.blog.articleNum].fields.mainImage.fields.file.url})`, backgroundPosition: 'center',
+      let time = "";
+      time = this.props.blog.posts[this.props.blog.articleNum].sys.createdAt;
+      time = moment(time).format('M.D.YY');
+      time = <p id="latestTime" className="txt-shadow font-weight-bolder">{time}</p>
+
+      return(
+        <div>
+        <Jumbotron className="text-center" id="blogJumbotron" style={{backgroundImage: `url(${this.props.blog.posts[this.props.blog.articleNum].fields.mainImage.fields.file.url})`, backgroundPosition: 'center',
             backgroundSize: 'cover',
             backgroundRepeat: 'no-repeat'}} fluid>
             <h1 className="blogTitle display-1">{this.props.blog.posts[this.props.blog.articleNum].fields.title}</h1>
+            {time}
+
         </Jumbotron>
+      </div>
+      )
+    }
+  }
+  
+  render() {
+
+    
+
+    return (
+      <div>
+        {this.loading()}
       </div>
     )
   }
