83d527193674d30c77996462a139880fab831d43
Jarred Sutton <solderedcircuit@gmail.com>
2020-01-30 23:33:31 +0000

    Updated initialState.js with dispatch call

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -6,10 +6,14 @@ export default {
       posts: [],
       articleNum: 0,
       alterArticleState: (e) => {
+				let updatedArticleNum = 0;
+				updatedArticleNum = $(e.target).attr('articlenum');
         // e.preventDefault()
         // console.log('e.target:', e.target)
         console.log('e.articleNum:', $(e.target).attr('articlenum'))
         // this.props.history.push('/Blog' + `${this.props.path}`);      } 
+				let action = {articleNum: updatedArticleNum} 
+				store.dispatch(action)
     }
   }
 }
