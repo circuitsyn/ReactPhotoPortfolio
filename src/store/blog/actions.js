3a94d321f5ab07af5d8b6b582afd18164d08421f
CircuitSyn <circuitsyn@gmail.com>
2020-01-31 11:27:57 -0800

    ripped out types and its variables

--- src/store/blog/actions.js
+++ src/store/blog/actions.js
@@ -1,12 +1,12 @@
 /**
 * Blog Actions
 */
-import * as types from './types';
+// import * as types from './types';
 
 export function blogLoading(isLoading = true) {
-  return { type: types.BLOG_LOADING, isLoading}
+  return { type: 'BLOG_LOADING', isLoading}
 }
 
 export function loadBlogSuccess(posts) {
-  return { type: types.LOAD_BLOG_POSTS_SUCCESS, posts}
+  return { type: 'LOAD_BLOG_POSTS_SUCCESS', posts}
 }
--- src/store/blog/reducer.js
+++ src/store/blog/reducer.js
@@ -2,16 +2,16 @@
 * Blog Reducer
 */
 import initialState from '../../store/initialState'
-import * as types from './types'
+// import * as types from './types'
 
 export default function blogReducer(state = initialState.blog, action) {
   switch (action.type) {
-    case types.BLOG_LOADING:
+    case 'BLOG_LOADING':
       return {
         ...state,
         loading: action.isLoading
       }
-    case types.LOAD_BLOG_POSTS_SUCCESS:
+    case 'LOAD_BLOG_POSTS_SUCCESS':
       return {
         ...state,
         posts: action.posts,
