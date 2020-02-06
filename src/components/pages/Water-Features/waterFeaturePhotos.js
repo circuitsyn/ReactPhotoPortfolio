d9c1a3950394592614ec0ee89562ac44556ebd10
CircuitSyn <circuitsyn@gmail.com>
2020-02-05 21:08:23 -0800

    images updated for water features

--- src/components/pages/Water-Features/waterFeaturePhotos.js
+++ src/components/pages/Water-Features/waterFeaturePhotos.js
@@ -2,68 +2,128 @@ export const waterFeaturePhotos = [
     {
       src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall01_HIGH.jpg",
       thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall01_LOW.jpg             ",
-      width: 867,
-      height: 1300
+      width: 3,
+      height: 4
     },
     {
       src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall02_HIGH.jpg",
       thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall02_LOW.jpg",
-      width: 1300,
-      height: 863
+      width: 4,
+      height: 3
     },
     {
       src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall03_HIGH.jpg",
       thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall03_LOW.jpg",
-      width: 863,
-      height: 1300
+      width: 3,
+      height: 4
     },
     {
       src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall05_HIGH.jpg",
       thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall05_LOW.jpg",
-      width: 1300,
-      height: 865
+      width: 4,
+      height: 3
     },
     {
       src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall04_HIGH.jpg",
       thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall04_LOW.jpg",
-      width: 863,
-      height: 1300
+      width: 3,
+      height: 4
     },
     { 
       src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall11_HIGH.jpg",
       thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall11_LOW.jpg",
-      width: 1300,
-      height: 867
+      width: 4,
+      height: 3
     },
     {
       src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall07_HIGH.jpg",
       thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall07_LOW.jpg",
-      width: 863,
-      height: 1300
+      width: 3,
+      height: 4
     },
     {
       src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall09_HIGH.jpg",
       thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall09_LOW.jpg",
-      width: 1300,
-      height: 867
+      width: 4,
+      height: 3
     },
     {
       src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall08_HIGH.jpg",
       thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall08_LOW.jpg",
-      width: 863,
-      height: 1300
+      width: 3,
+      height: 4
     },
     {
       src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall10_HIGH.jpg",
       thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall10_LOW.jpg",
-      width: 1300,
-      height: 867
+      width: 4,
+      height: 3
     },
     {
       src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall06_HIGH.jpg",
       thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall06_LOW.jpg",
-      width: 863,
-      height: 1300
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall12_HIGH.jpg",
+      thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall12_LOW.jpg",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall13_HIGH.jpg",
+      thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall13_LOW.jpg",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall14_HIGH.jpg",
+      thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall14_LOW.jpg",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall15_HIGH.jpg",
+      thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall15_LOW.jpg",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall19_HIGH.jpg",
+      thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall19_LOW.jpg",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall16_HIGH.jpg",
+      thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall16_LOW.jpg",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall20_HIGH.jpg",
+      thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall20_LOW.jpg",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall17_HIGH.jpg",
+      thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall17_LOW.jpg",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall21_HIGH.jpg",
+      thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall21_LOW.jpg",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall18_HIGH.jpg",
+      thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall18_LOW.jpg",
+      width: 3,
+      height: 4
     }
   ];
   
