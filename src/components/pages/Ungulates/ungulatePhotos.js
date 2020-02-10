4675ab194681c3e0afa9b9cbaa7e51e9e358cc23
CircuitSyn <circuitsyn@gmail.com>
2020-02-10 00:14:53 -0800

    mammals updated

--- src/components/pages/Ungulates/ungulatePhotos.js
+++ src/components/pages/Ungulates/ungulatePhotos.js
@@ -1,129 +1,129 @@
 export const ungulatePhotos = [
   {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall01_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall01_LOW.jpg",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall02_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall02_LOW.jpg",
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals01_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals01_LOW.jpg",
     width: 4,
     height: 3
   },
   {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall03_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall03_LOW.jpg",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall05_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall05_LOW.jpg",
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals05_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals05_LOW.jpg",
     width: 4,
     height: 3
   },
   {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall04_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall04_LOW.jpg",
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals03_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals03_LOW.jpg",
     width: 3,
     height: 4
+  },
+  {
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals12_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals12_LOW.jpg",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals06_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals06_LOW.jpg",
+    width: 4,
+    height: 3
   },
   { 
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall11_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall11_LOW.jpg",
-    width: 4,
-    height: 3
-  },
-  {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall07_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall07_LOW.jpg",
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals07_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals07_LOW.jpg",
     width: 3,
     height: 4
   },
   {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall09_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall09_LOW.jpg",
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals08_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals08_LOW.jpg",
     width: 4,
     height: 3
   },
   {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall08_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall08_LOW.jpg",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall10_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall10_LOW.jpg",
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals09_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals09_LOW.jpg",
     width: 4,
     height: 3
   },
   {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall06_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall06_LOW.jpg",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall12_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall12_LOW.jpg",
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals10_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals10_LOW.jpg",
     width: 4,
     height: 3
   },
   {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall13_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall13_LOW.jpg",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall14_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall14_LOW.jpg",
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals11_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals11_LOW.jpg",
     width: 4,
     height: 3
   },
   {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall15_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall15_LOW.jpg",
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals04_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals04_LOW.jpg",
     width: 3,
     height: 4
   },
   {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall19_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall19_LOW.jpg",
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals13_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals13_LOW.jpg",
     width: 4,
     height: 3
   },
   {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall16_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall16_LOW.jpg",
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals14_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals14_LOW.jpg",
     width: 3,
     height: 4
   },
   {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall20_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall20_LOW.jpg",
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals15_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals15_LOW.jpg",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals16_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals16_LOW.jpg",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals17_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals17_LOW.jpg",
     width: 4,
     height: 3
   },
   {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall17_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall17_LOW.jpg",
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals18_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals18_LOW.jpg",
     width: 3,
     height: 4
   },
   {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall21_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall21_LOW.jpg",
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals19_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals19_LOW.jpg",
     width: 4,
     height: 3
   },
   {
-    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall18_HIGH.jpg",
-    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Water_Features/waterfall18_LOW.jpg",
-    width: 3,
-    height: 4
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals20_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals20_LOW.jpg",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals21_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals21_LOW.jpg",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals22_HIGH.jpg",
+    thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Mammals/mammals22_LOW.jpg",
+    width: 4,
+    height: 3
   }
   ];
   
