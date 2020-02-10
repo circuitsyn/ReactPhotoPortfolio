15766c009229c28b7ebfe60ac91e8ced5f24ac72
CircuitSyn <circuitsyn@gmail.com>
2020-02-09 22:26:09 -0800

    updated dimensions of for panoramas

--- src/components/pages/Panoramas/panoramasPhotos.js
+++ src/components/pages/Panoramas/panoramasPhotos.js
@@ -2,44 +2,44 @@ export const panoramasPhotos = [
   {
     src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Panoramas/panorama01_HIGH.jpg",
     thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Panoramas/panorama01_LOW.jpg",
-    width: 3,
-    height: 4
+    width: 8,
+    height: 2
   },
   {
     src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Panoramas/panorama02_HIGH.jpg",
     thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Panoramas/panorama02_LOW.jpg",
-    width: 4,
-    height: 3
+    width: 8,
+    height: 2
   },
   {
     src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Panoramas/panorama03_HIGH.jpg",
     thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Panoramas/panorama03_LOW.jpg",
-    width: 3,
-    height: 4
+    width: 8,
+    height: 3
   },
   {
     src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Panoramas/panorama04_HIGH.jpg",
     thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Panoramas/panorama04_LOW.jpg",
-    width: 4,
-    height: 3
+    width: 2,
+    height: 8
   },
   {
     src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Panoramas/panorama05_HIGH.jpg",
     thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Panoramas/panorama05_LOW.jpg",
-    width: 3,
-    height: 4
+    width: 9,
+    height: 2
   },
   { 
     src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Panoramas/panorama06_HIGH.jpg",
     thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Panoramas/panorama06_LOW.jpg",
-    width: 4,
-    height: 3
+    width: 8,
+    height: 2
   },
   {
     src: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Panoramas/panorama07_HIGH.jpg",
     thumbnail: "https://js-react-photo-portfolio.s3-us-west-2.amazonaws.com/PhotoPortfolio/Panoramas/panorama07_LOW.jpg",
-    width: 3,
-    height: 4
+    width: 8,
+    height: 2
   }
 ];
   
